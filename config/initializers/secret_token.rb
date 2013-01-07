58bb890ba9b26dfcab698b24582a0a50ff8fc675
Jimi <jimi@jimi.(none)>
2013-01-07 16:39:48 -0300

    1er commit. Aplicaci{on en pelotas.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synchronizer::Application.config.secret_token = 'c8e78b4bba7f12928b4c0637a742b6cd1718a27912de32008b1c0942d34e697a80f4918e29d77f09c8469bf3e81b3b3e4022d526eaf280528ad1123747529fb2'
