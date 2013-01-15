622818e10a86dd4cbd48adf6f2ac6486069e119f
Jimi <jimi@jimi.(none)>
2013-01-15 16:27:14 -0300

    Al fin se llega a lista de productos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,35 @@ require "rubygems"
 require "mechanize"
 
 def sync
+  text = ""
+  url_main = 'https://pyme.defontana.com/sistema/main.asp'
+  url_login = 'https://pyme.defontana.com/sistema/login.asp'
+  url_toolbar = 'https://pyme.defontana.com/sistema/toolbar.asp '
+  url_main_loged = 'https://pyme.defontana.com/sistema/Matprod/IniMain.asp'
+  url_inventario = 'https://pyme.defontana.com/sistema/inven/LisArticulos.ASP?WCI=wiLisArticulos&WCE=form1&WCU'
   agent = Mechanize.new
+  agent.user_agent_alias= 'Windows IE 6'
   begin
-    page = agent.get('https://pyme.defontana.com/sistema/main.asp')
-    Rails.logger.debug "PAGE => #{page.content()}"
-    form = page.frame_with(:name => 'main').content().iframe.content().form
-    Rails.logger.debug "FORM => #{form}"
-    # Se llena formulario
+    page = agent.get(url_main)
+    form = page.frame_with(:name => 'main').click.iframe.click.form
     form.txtCliente = 'sonosur'
     form.txtUsuario = 'ddf'
     form.txtPassword = 'dilmanddf'
-    Rails.logger.debug "FORM_MODIFICADO => #{form}"
-    # Submit de formulario
-    page = agent.submit(form, form.buttons.first)
-    # page = agent.get('https://pyme.defontana.com/sistema/inven/LisArticulos.ASP?WCI=wiEditar&WCE=inven@')
-    Rails.logger.debug "PAGINA INVENTARIO => #{page.content}"
-    redirect_to "http://google.cl"
+    agent.submit(form, form.button)
+    # agent.cookie_jar.save_as('cookie.yml')
+
+    page = agent.get(url_main_loged)
+    page = agent.post(url_inventario)
+    page = page.links[0].click
+    form = page.form
+    page = agent.submit(form, form.button_with(:name => 'I1'))
+    logger.debug { "\n\n\n\n\n#{page.content}" }
+    # Y tenemos la página donde se muestran TODOS los productos! YEAH!
+    @text = "#{@text}\n\n TABLA_PRODUCTOS =>#{page.content.to_xml}"
+
   rescue Mechanize::ResponseReadError => e
     Rails.logger.debug "ERROR => #{e.inspect}"
   end
 end
-
   protect_from_forgery
 end
