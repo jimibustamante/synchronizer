9975316ad3cffd2bf6f09e3a3b106b1852f4e37c
Jimi <jimi@jimi.(none)>
2013-01-16 20:48:44 -0300

    Se establece cómo cambir de página en la lista de productos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,20 @@ def sync
     page = page.links[0].click
     form = page.form
     page = agent.submit(form, form.button_with(:name => 'I1'))
+    # Para cambiar de página se debe enviar un POST con los parámetros que aparecen debajo.
+    # La paginación mostrada corresponde a i+1.
+    # Por ejemplo, para ver la página 4, i=3
+    # TODO => Iterar sobre 'i' para leer el contenido de los productos.
+    i=6
+    page = agent.post(url_inventario,{"DirPag" => "1","NumPag" => i.to_s,"hidUsaCodBarGS1128" => "N","pagSgte" => "Siguiente >>"})
+
+    # form = page.forms.first
+    # agent.submit(form, form.button_with(:name => 'pagSgte'))
+    # page = agent.current_page
     logger.debug { "\n\n\n\n\n#{page.content}" }
-    
+
     # Y tenemos la página donde se muestran TODOS los productos! YEAH!
-    @text = "#{@text}\n\n TABLA_PRODUCTOS =>#{page.content.to_xml}"
+    @text = "#{@text}\n\n TABLA_PRODUCTOS =>#{page.content}"
 
   rescue Mechanize::ResponseReadError => e
     Rails.logger.debug "ERROR => #{e.inspect}"
