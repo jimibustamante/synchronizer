4c7dfc1f6036533b92f08546ff9e551ed53d0259
Jimi <jimi@jimi.(none)>
2013-01-17 14:07:34 -0300

    Intentando sacar el máximo de páginas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,32 +9,34 @@ def sync
   url_toolbar = 'https://pyme.defontana.com/sistema/toolbar.asp '
   url_main_loged = 'https://pyme.defontana.com/sistema/Matprod/IniMain.asp'
   url_inventario = 'https://pyme.defontana.com/sistema/inven/LisArticulos.ASP?WCI=wiLisArticulos&WCE=form1&WCU'
-  agent = Mechanize.new
-  agent.user_agent_alias= 'Windows IE 6'
+  @agent = Mechanize.new
+  @agent.user_agent_alias= 'Windows IE 6'
 
   begin
-    page = agent.get(url_main)
+    page = @agent.get(url_main)
     form = page.frame_with(:name => 'main').click.iframe.click.form
     form.txtCliente = 'sonosur'
     form.txtUsuario = 'ddf'
     form.txtPassword = 'dilmanddf'
-    agent.submit(form, form.button)
+    @agent.submit(form, form.button)
 
-    page = agent.get(url_main_loged)
-    page = agent.post(url_inventario)
+    page = @agent.get(url_main_loged)
+    page = @agent.post(url_inventario)
     page = page.links[0].click
     form = page.form
-    page = agent.submit(form, form.button_with(:name => 'I1'))
+    page = @agent.submit(form, form.button_with(:name => 'I1'))
     # Para cambiar de página se debe enviar un POST con los parámetros que aparecen debajo.
     # La paginación mostrada corresponde a i+1.
     # Por ejemplo, para ver la página 4, i=3
     # TODO => Iterar sobre 'i' para leer el contenido de los productos.
     i=6
-    page = agent.post(url_inventario,{"DirPag" => "1","NumPag" => i.to_s,"hidUsaCodBarGS1128" => "N","pagSgte" => "Siguiente >>"})
+    logger.debug { "TEXTO => #{page.search('html>body>form>table>tbody>tr>td')}" }
+    
+    page = @agent.post(url_inventario,{"DirPag" => "1","NumPag" => i.to_s,"hidUsaCodBarGS1128" => "N","pagSgte" => "Siguiente >>"})
 
     # form = page.forms.first
-    # agent.submit(form, form.button_with(:name => 'pagSgte'))
-    # page = agent.current_page
+    # @agent.submit(form, form.button_with(:name => 'pagSgte'))
+    # page = @agent.current_page
     logger.debug { "\n\n\n\n\n#{page.content}" }
 
     # Y tenemos la página donde se muestran TODOS los productos! YEAH!
