7f4ac535bee7531a1cc928d8143613108c6d866d
Jimi <jimi@jimi.(none)>
2013-01-18 18:55:55 -0300

    Se navega por los productos, se parsea el ID y se busca el producto en base de datos cecaudio mostrando el STOCK de cada uno.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ def sync
   url_inventario = 'https://pyme.defontana.com/sistema/inven/LisArticulos.ASP?WCI=wiLisArticulos&WCE=form1&WCU'
   @agent = Mechanize.new
   @agent.user_agent_alias= 'Windows IE 6'
-
+  @product = Product.new
   begin
     page = @agent.get(url_main)
     form = page.frame_with(:name => 'main').click.iframe.click.form
@@ -31,12 +31,44 @@ def sync
     # TODO => Iterar sobre 'i' para leer el contenido de los productos.
     page_number = page.search('/html/body/form/table[7]/tr/td[1]').text.to_s.last.to_i
 
+    # Iteramos sobre cada página
     for i in 0..(page_number - 1)
       page = @agent.post(url_inventario,{"DirPag" => "1","NumPag" => i.to_s,"hidUsaCodBarGS1128" => "N","pagSgte" => "Siguiente >>"})
       # Acá se deben leer los productos, tomar su ID y ejecutar método para actualizar datos que se encuentra en el modelo Product
       logger.debug { "i => #{i}" }
       logger.debug { "#{page.search('/html/body/form/table[7]/tr/td[4]')}" }
-      logger.debug { "#{page.content}\n\n\n\n" }
+
+      j=1
+      until page.search('/html/body/form/ul/li['+j.to_s+']/a').text.blank? == true do #NIVEL A
+        # logger.debug { "NIVEL A:#{page.search('/html/body/form/ul/li['+j.to_s+']/a').text}" }
+        k = 1
+        until page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/a').text.blank? == true do #NIVEL B
+          # logger.debug { "  NIVEL B:#{page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/a').text}" }
+          l=1
+          until page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/ul/li['+l.to_s+']/a').text.blank? == true do #NIVEL C - Productos
+            # logger.debug { "    NIVEL C: #{page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/ul/li['+l.to_s+']/a').text}" }
+            m=1
+            until page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/ul/li['+l.to_s+']/ul/table['+m.to_s+']/tr/td[1]/font/a').text.blank? == true
+              lectura = page.search('/html/body/form/ul/li['+j.to_s+']/ul/li['+k.to_s+']/ul/li['+l.to_s+']/ul/table['+m.to_s+']/tr/td[1]/font/a').text
+              logger.debug { "PRODUCTO:#{lectura}" }
+              id_product = lectura.split('-').first.split('.').last.to_i
+              logger.debug { "  ID => #{id_product}" }
+              @product = Product.find id_product
+              if @product.nil?
+                logger.debug { "  Producto NO SE ENCONTRO" }
+              else
+                logger.debug { "  STOCK => #{@product.products_quantity.to_s}" }
+              end
+              m+=1
+            end
+            l+=1
+          end
+          k+=1
+        end
+        j+=1
+      end
+
+      # logger.debug { "#{page.content}\n\n\n\n" }
       
     end
 
